54bd9808cbbe551a5b1615dd62844b689fbe246b
hphothong <hphothong@gmail.com>
2019-10-06 13:31:38 -0600

    attempting to fix the unit test

--- daily-coding-problems/src/main/java/com/hphothong/coding/challenges/problem1/Problem1.java
+++ daily-coding-problems/src/main/java/com/hphothong/coding/challenges/problem1/Problem1.java
@@ -1,9 +1,25 @@
 package com.hphothong.coding.challenges.problem1;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class Problem1 {
 
-    public void problem() {
+    public boolean problem1(List<Integer> numberList, int k) {
+        HashMap<Integer, Integer> differenceToNumberMap = new HashMap<>();
 
+        for (Integer number : numberList) {
+            if (number != null) {
+                int difference = k - number;
+                if (differenceToNumberMap.containsKey(difference)) {
+                    return true;
+                } else {
+                    differenceToNumberMap.put(difference, number);
+                }
+            }
+        }
+
+        return false;
     }
 
 }
--- daily-coding-problems/src/test/java/com/hphothong/coding/challenges/problem1/Problem1Test.java
+++ daily-coding-problems/src/test/java/com/hphothong/coding/challenges/problem1/Problem1Test.java
@@ -1,13 +1,30 @@
-package com.hphothong.challenges.problem1;
+package com.hphothong.coding.challenges.problem1;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import java.util.List;
+import com.google.common.collect.Lists;
 
 public class Problem1Test {
 
+    private Problem1 instance;
+
+    @Override
+    public void setUp() {
+        instance = new Problem1();
+    }
+
     @Test
     public void testProblem1() {
-        assertEquals(true, true);
+        // Given a number list and number k
+        List<Integer> numberList = Lists.newArrayList(10, 15, 3, 7);
+        int k = 17;
+
+        // When testing if two of the values in the list equal the value k
+        boolean actualResult = instance.problem1(numberList, k);
+
+        // Then 'true' should be returned
+        assertTrue(actualResult);
     }
 }
