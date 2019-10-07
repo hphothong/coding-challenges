7b7bd42183e43aab520dbc30619bb88773977aec
hphothong <hphothong@gmail.com>
2019-10-06 20:31:09 -0600

    Adding javadoc to problem 1

---
+++ daily-coding-problems/src/test/java/com/hphothong/coding/challenges/problem2/Problem2Test.java
@@ -0,0 +1,21 @@
+package com.hphothong.coding.challenges.problem2;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class Problem2Test {
+
+    private Problem2 instance;
+
+    @Before
+    public void setUp() {
+        instance = new Public2();
+    }
+
+    @Test
+    public void testProblem2() {
+        assertTrue(true);
+    }
+
+}
